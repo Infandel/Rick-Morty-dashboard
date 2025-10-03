16f91fafb7253245901151ed2f0cef0d159e0794
Infandel <tomahawk92@gmail.com>
2025-10-04 03:16:40 +0500

    Added basic router workaround for wildcard routes.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,19 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import { Layout } from "@widgets/layout"
-import { PostsPage } from "@pages/posts"
-import { CharactersPage } from "@pages/characters"
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Layout } from '@widgets/layout';
+import { PostsPage } from '@pages/posts';
+import { CharactersPage } from '@pages/characters';
 
 export function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Navigate to="/posts" replace />} />
-          <Route path="posts" element={<PostsPage />} />
-          <Route path="characters" element={<CharactersPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					<Route index element={<Navigate to='/posts' replace />} />
+					<Route path='posts' element={<PostsPage />} />
+					<Route path='characters' element={<CharactersPage />} />
+					<Route path='*' element={<Navigate to='/posts' replace />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
